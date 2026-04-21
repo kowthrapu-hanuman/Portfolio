fb8f501998b26f53744f7872230d54a7dd0a2e4a
Kowthrapu Hanuman <hanuman.kowthrapu2005@gmail.com>
2026-04-21 16:37:49 +0530

    Delete flask_app/venv1 directory

--- flask_app/venv1/portfolio/static/js/aos.js
+++
@@ -1,5 +0,0 @@
-<div class="project-card" data-aos="flip-left">
-  <h3>Flight Satisfaction Prediction</h3>
-  <p>Machine Learning model deployed with Flask.</p>
-  <a href="#" class="btn">View Code</a>
-</div>
